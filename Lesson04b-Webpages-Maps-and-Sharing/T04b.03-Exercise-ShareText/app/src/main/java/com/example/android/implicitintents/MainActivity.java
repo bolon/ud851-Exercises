28a9cf10d477fbcf28fcdb45f31a38c5a7f58a20
bolon <fsimbolon@dyned.com>
2017-06-16 21:32:58 +0700

    T04b.03-Exercise-ShareText

--- Lesson04b-Webpages-Maps-and-Sharing/T04b.03-Exercise-ShareText/app/src/main/java/com/example/android/implicitintents/MainActivity.java
+++ Lesson04b-Webpages-Maps-and-Sharing/T04b.03-Exercise-ShareText/app/src/main/java/com/example/android/implicitintents/MainActivity.java
@@ -18,6 +18,7 @@ package com.example.android.implicitintents;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
@@ -51,9 +52,7 @@ public class MainActivity extends AppCompatActivity {
         String addressString = "1600 Amphitheatre Parkway, CA";
 
         Uri.Builder builder = new Uri.Builder();
-        builder.scheme("geo")
-                .path("0,0")
-                .query(addressString);
+        builder.scheme("geo").path("0,0").query(addressString);
         Uri addressUri = builder.build();
 
         showMap(addressUri);
@@ -67,9 +66,10 @@ public class MainActivity extends AppCompatActivity {
      */
     public void onClickShareTextButton(View v) {
         // TODO (5) Specify a String you'd like to share
+        String toShare = "test text";
 
         // TODO (6) Replace the Toast with shareText, passing in the String from step 5
-        Toast.makeText(this, "TODO: Share text when this is clicked", Toast.LENGTH_LONG).show();
+        shareText(toShare);
     }
 
     /**
@@ -77,15 +77,11 @@ public class MainActivity extends AppCompatActivity {
      * similar to what I've done above. You can view a list of implicit Intents on the Common
      * Intents page from the developer documentation.
      *
-     * @see <http://developer.android.com/guide/components/intents-common.html/>
-     *
      * @param v Button that was clicked.
+     * @see <http://developer.android.com/guide/components/intents-common.html/>
      */
     public void createYourOwn(View v) {
-        Toast.makeText(this,
-                "TODO: Create Your Own Implicit Intent",
-                Toast.LENGTH_SHORT)
-                .show();
+        Toast.makeText(this, "TODO: Create Your Own Implicit Intent", Toast.LENGTH_SHORT).show();
     }
 
     /**
@@ -119,7 +115,7 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * This method will fire off an implicit Intent to view a location on a map.
-     *
+     * <p>
      * When constructing implicit Intents, you can use either the setData method or specify the
      * URI as the second parameter of the Intent's constructor,
      * as I do in {@link #openWebPage(String)}
@@ -145,10 +141,18 @@ public class MainActivity extends AppCompatActivity {
 
     // TODO (1) Create a void method called shareText that accepts a String as a parameter
     // Do steps 2 - 4 within the shareText method
-
+    void shareText(String s) {
         // TODO (2) Create a String variable called mimeType and set it to "text/plain"
-
+        String mimeType = "text/plain";
         // TODO (3) Create a title for the chooser window that will pop up
-
+        String title = "Share text";
         // TODO (4) Use ShareCompat.IntentBuilder to build the Intent and start the chooser
+        ShareCompat.IntentBuilder.from(this)
+                .setChooserTitle(title)
+                .setType(mimeType)
+                .setText(s)
+                .startChooser();
+    }
+
+
 }
